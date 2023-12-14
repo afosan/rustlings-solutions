4cad775ec24708c221a6981967b8ceefdd3c7c30
afozkalay <afozkalay@skiff.com>
2023-12-14 07:24:27 +0300

    add solutions

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -40,10 +40,25 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+// I AM DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let vals: Vec<&str> = s.split(",").collect();
+        if vals.len() < 2 {
+            return Person::default();
+        }
+        let name = vals[0].to_string();
+        if name.len() == 0 {
+            return Person::default();
+        }
+        match vals[1].parse::<usize>() {
+            Ok(age) => return Person{ name, age },
+            ParseIntError => return Person::default(),
+        }
     }
 }
 
--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+// I AM DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,23 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(Self::Err::Empty);
+        }
+
+        let vals = s.split(",").collect::<Vec<&str>>();
+        if vals.len() != 2 {
+            return Err(Self::Err::BadLen);
+        }
+
+        let name = vals[0].to_string();
+        if name.len() == 0 {
+            return Err(Self::Err::NoName);
+        }
+
+        let age = vals[1].parse::<usize>().map_err(|e| Self::Err::ParseInt(e))?;
+
+        Ok(Person{ name, age })
     }
 }
 
